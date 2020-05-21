29e56f77ca2aa7b0e0036579a717f5e2f0ee2867
shaolin-wu <wslsdust@163.com>
2020-05-21 20:23:36 +0800

    modify index.ts and add editable-html project

--- js-demo/form-validator/form-validator-ts/index.ts
+++ js-demo/form-validator/form-validator-ts/index.ts
@@ -4,9 +4,34 @@ const emailTs: Element = document.getElementById("email")
 const passwordTs: Element = document.getElementById("password")
 const confirmPasswordTs: Element = document.getElementById("confirmPassword")
 
-function showErrorInfo(inputNode: Element , message: string): void {
+/**
+ * 显示form item的错误信息
+ * @param inputNode 
+ * @param message 
+ */
+function showErrorInfo(inputNode: HTMLElement , message: string): void {
   const parentNode: HTMLElement = inputNode.parentElement
   parentNode.className = "form-control error"
   const small: HTMLElement  = parentNode.querySelector("small")
   small.innerHTML = message
+}
+
+function showSuccessInfo(inputNode: HTMLElement): void {
+  const parentNode: HTMLElement = inputNode.parentElement
+  parentNode.className = "form-control success"
+}
+
+function checkRequiredTs(inputNodeArr: Array<Element>): void {
+  inputNodeArr.forEach((inputNode: HTMLInputElement) => {
+    if (inputNode.value.trim() === "") {
+      showErrorInfo(inputNode, `${getKeywords}不能为空`)
+    } else {
+      showSuccessInfo(inputNode)
+    }
+  })
+}
+
+function getKeywords(inputNode: HTMLElement): string {
+  let requiredWord: string = inputNode.placeholder
+  return requiredWord.slice(3)
 }
