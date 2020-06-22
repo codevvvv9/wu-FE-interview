7c40f9ab569d02518a031c598bf4ce470f3cefe7
wushaolin <15265249516@163.com>
2020-06-23 00:14:19 +0800

    What:  add vuex-demo with vue-cli4
    Why:
    
    *
    
    How:
    
    *

---
+++ vue-learning/vuex-demo/src/App.vue
@@ -0,0 +1,59 @@
+<template>
+  <div id="app">
+    <p>{{ count }}</p>
+    <p>{{ completedTodos }}</p>
+    <p>{{ completedTodosCount }}</p>
+    <p>{{ getTodosById(0) }}</p>
+  </div>
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld.vue";
+import { mapState } from "vuex";
+export default {
+  name: "App",
+  components: {
+    HelloWorld
+  },
+  // computed: {
+  //   count() {
+  //     return this.$store.state.count
+  //   },
+  //   todos() {
+  //     return this.$store.state.todos
+  //   }
+  // }
+  // computed: mapState({
+  //   count: state => state.count,
+  //   todos: state => state.todos
+  // })
+  //state && computed
+  // computed: mapState(["count", "todos"])
+  computed: {
+    count() {
+      //执行的是count的返回值
+      return this.$store.getters.count;
+    },
+    completedTodos() {
+      return this.$store.getters.completedTodos;
+    },
+    completedTodosCount() {
+      return this.$store.getters.completedTodosCount
+    },
+    getTodosById() {
+      return this.$store.getters.getTodosById
+    }
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ vue-learning/vuex-demo/src/store/index.js
@@ -0,0 +1,50 @@
+import Vue from "vue";
+import Vuex from "vuex";
+
+Vue.use(Vuex);
+
+export default new Vuex.Store({
+  state: {
+    count: 22,
+    todos: [
+      {
+        id: 0,
+        title: "demo00",
+        completed: true,
+      },
+      {
+        id: 1,
+        title: "demo01",
+        completed: false,
+      },
+      {
+        id: 2,
+        title: "demo022",
+        completed: true,
+      },
+    ],
+  },
+  mutations: {},
+  actions: {},
+  modules: {},
+  getters: {
+    count: (state) => ++state.count,
+    completedTodos: (state) => {
+      return state.todos.filter(todo => todo.completed);
+    },
+    completedTodosCount: (state, getters) => {
+      return getters.completedTodos.length
+    },
+    // getTodosById(state) {
+    //   return (id) => {
+    //     return state.todos.find(todo => {
+    //       return todo.id === id
+    //     })
+    //   }
+    // }
+    //上述写法可以使用连续箭头函数 高级函数柯里化来实现
+    getTodosById: state => id => {
+      return state.todos.find(todo => todo.id === id)
+    }
+  },
+});
