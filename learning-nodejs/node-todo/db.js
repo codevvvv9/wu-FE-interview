2c5db7565c4c9e6dcee8728e48bdbc66d1211113
wushaolin <15265249516@163.com>
2020-07-12 17:40:42 +0800

    What:  Code encapsulation
    Why:
    
    *
    
    How:
    
    *

---
+++ learning-nodejs/node-todo/db.js
@@ -0,0 +1,48 @@
+/**
+ * @author wushaolin
+ * @email wslsdust@163.com
+ * @create date 2020-07-12 17:24:24
+ * @modify date 2020-07-12 17:24:24
+ * @desc [代码封装，数据库的读写功能]
+ */
+
+const fs = require("fs");
+const path = require("path");
+//获取家目录 ~
+const homedir = require("os").homedir();
+//如果自己设置了私有的Home就用私有的
+const home = process.env.HOME || homedir;
+//在你的系统家目录下的 ~/.todo
+const dbPath = path.join(home, ".todo");
+/**
+ * 构建db对象，做任务的读写功能
+ */
+const db = {
+  read(path = dbPath) {
+    return new Promise((resolve, reject) => {
+      fs.readFile(path, { flag: "a+" }, (readError, data) => {
+        if (readError) return reject(readError);
+        //存任务的数组
+        let list;
+        try {
+          list = JSON.parse(data.toString());
+        } catch (error) {
+          list = [];
+          resolve(list)
+        }
+      });
+    });
+  },
+  write(data, path = dbPath) {
+    return new Promise((resolve, reject) => { 
+      const taskString = JSON.stringify(data);
+      //把添加的任务写入到~/.todo中,taskString + "\n" 用来换行
+      fs.writeFile(path, taskString + "\n", (writeError) => {
+        if (writeError) return reject(writeError)
+        resolve()
+      });
+    })
+  },
+};
+
+module.exports = db
--- learning-nodejs/node-todo/index.js
+++ learning-nodejs/node-todo/index.js
@@ -1,43 +1,10 @@
-const fs = require('fs')
-const path = require("path")
-//获取家目录 ~
-const homedir = require('os').homedir();
-console.log("homedir", homedir)
-//如果自己设置了私有的Home就用私有的
-const home = process.env.HOME || homedir
-console.log("home", home)
-//在你的系统家目录下的 ~/.todo
-const dbPath = path.join(home, ".todo")
-console.log("dbPath", dbPath)
+const db = require("./db.js")
 
-module.exports.add = (title) => {
-  console.log('title is', title);
+module.exports.add = async (title) => {
   //1、读取之前的任务
-  fs.readFile(dbPath, {flag: "a+"}, (readError, data) => {
-    if (readError) {
-      //如果读取错误，打印读取错误
-      console.log('read file error is: ', readError);
-    } else {
-      //存任务的数组
-      let list
-      try {
-        list = JSON.parse(data.toString())
-      } catch (error) {
-        list = []
-      }
-
-      console.log('list is: ', list);
-      const task = {
-        title: title,
-        done: false
-      }
-      list.push(task)
-      console.log('after list push is: ', list);
-      const taskString = JSON.stringify(list)
-      //把添加的任务写入到~/.todo中,taskString + "\n" 用来换行
-      fs.writeFile(dbPath, taskString + "\n", (writeError) => {
-        console.log('write task error is: ', writeError);
-      })
-    }
-  })
+  const list = await db.read()
+  //2、往任务列表里面增加任务项
+  list.push({title, done: false})
+  //3、存储任务到文件中
+  await db.write(list)
 }
