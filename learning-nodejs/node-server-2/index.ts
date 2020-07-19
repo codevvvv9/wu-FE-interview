fa42887b4c0801d74361ffffc934c457eebc6992
wushaolin <15265249516@163.com>
2020-07-19 18:13:17 +0800

    What:  target1: Match different paths
    Why:
    
    *
    
    How:
    
    *

--- learning-nodejs/node-server-2/index.ts
+++ learning-nodejs/node-server-2/index.ts
@@ -1,28 +1,36 @@
 import * as http from "http";
+import * as fs from "fs"
+import * as path from "path";
 
 const server = http.createServer();
-
-server.on('request', (request:http.IncomingMessage, response:http.ServerResponse) => {
-  console.log('request.method', request.method);
-  console.log('request.url', request.url);
-  console.log('request.headers', request.headers);
-  
-  // 监听post请求的
-  const array: Buffer[] = []
-  request.on('data', chunk => {
-    array.push(chunk)
-  })
-  request.on('end', () => {
-    const body = Buffer.concat(array).toString()
-    console.log('body is', body);
-    //接受数据完毕后可以更改response
-    response.statusCode = 400
-    response.setHeader('Content-Type', 'testtest')
-    response.write('1\n')
-    response.write('2\n')
-    response.write('3\n')
-    response.end('hi');
-  })
+const publicPath = path.resolve(__dirname, "public");
+server.on('request', (request: http.IncomingMessage, response: http.ServerResponse) => {
+  const { url } = request
+  switch (url) {
+    case "/index.html":
+      response.setHeader("Content-Type", "text/html; charset=utf-8")
+      fs.readFile(path.resolve(publicPath, "index.html"), (err, data) => {
+        if (err) throw err
+        response.end(data.toString())
+      })
+      break;
+    case "/style.css":
+      response.setHeader("Content-Type", "text/css; charset=utf-8")
+      fs.readFile(path.resolve(publicPath, "style.css"), (err, data) => {
+        if (err) throw err
+        response.end(data.toString())
+      })
+      break;
+    case "/main.js":
+      response.setHeader("Content-Type", "text/javascript; charset=utf-8")
+      fs.readFile(path.resolve(publicPath, "main.js"), (err, data) => {
+        if (err) throw err
+        response.end(data.toString())
+      })
+      break;
+    default:
+      break;
+  }
 })
 
 server.listen(8888, () => {
