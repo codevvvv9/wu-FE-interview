721af68d85eefe6c04a4d9db0e1b715e1642fe1b
wushaolin <15265249516@163.com>
2020-07-19 11:37:50 +0800

    What:  test get and post method in node-server2 demo
    Why:
    
    *
    
    How:
    
    *

--- learning-nodejs/node-server-2/README.md
+++ learning-nodejs/node-server-2/README.md
@@ -1,3 +1,9 @@
 1. yarn init -y
 2. touch index.ts
 3. yarn add --dev @types/node 安装node声明文件
+4. 引入http模块（webstorm自动引入，但是vscode需要配置`tsconfig.json`）
+5. http模块创建`server`
+6. 监听上述创建的`server`的request事件
+7. server.listen(port),服务器监听一个具体的端口
+8. 客户端模拟get访问数据（`curl -v http://localhost:port`）
+9. 客户端模拟post访问数据（`curl -v -d "dataContent" http://localhost:port`）
--- learning-nodejs/node-server-2/index.ts
+++ learning-nodejs/node-server-2/index.ts
@@ -1,3 +1,23 @@
 import * as http from "http";
 
 const server = http.createServer();
+
+server.on('request', (request:http.IncomingMessage, response:http.ServerResponse) => {
+  console.log('request.method', request.method);
+  console.log('request.url', request.url);
+  console.log('request.headers', request.headers);
+  
+  // 监听post请求的
+  const array: Buffer[] = []
+  request.on('data', chunk => {
+    array.push(chunk)
+  })
+  request.on('end', () => {
+    const body = Buffer.concat(array).toString()
+    console.log('body is', body);
+    response.end('hi');
+  })
+})
+
+server.listen(8888, () => {
+})
