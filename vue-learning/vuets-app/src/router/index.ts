7d3e6cde00ef8ad2ed7e498d22639fe7c7a82903
wushaolin <15265249516@163.com>
2020-08-11 23:21:25 +0800

    What:  refresh router.ts
    Why:
    
    *
    
    How:
    
    *

--- vue-learning/vuets-app/src/router/index.ts
+++ vue-learning/vuets-app/src/router/index.ts
@@ -1,23 +1,51 @@
 import Vue from 'vue'
 import VueRouter, { RouteConfig } from 'vue-router'
-import Home from '../views/Layout/Index.vue'
+import Layout from '../views/Layout/Index.vue';
 
 Vue.use(VueRouter)
-
-const routes: Array<RouteConfig> = [
+/**
+ * hidden: true          为true在左侧菜单栏显示，否则不显示
+ * name: "router-name"   路由名称 必写
+ * meta: {
+ *  title: "title"       对应路由在左侧菜单的标题名称
+ *  icon: "icon-class"   对应路由在左侧菜单的图标样式
+ * }
+ */
+export const asyncRouterMap = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    name: 'dashboard',
+    component: Layout,
+    redirect: "/home",
+    hidden: true,
+    children: [
+      {
+        path: "/home",
+        name: "home",
+        meta: {
+          title: "首页",
+          icon: "fa fa-home",
+        },
+        component: () => import("@/views/Home.vue")
+      }
+    ]
   },
   {
     path: '/login',
     name: 'login',
+    hidden: false,
+    meta: {
+      title: "系统登录",
+    },
     component: () => import("@/views/login/Login.vue")
   },
   {
     path: '/password',
     name: 'password',
+    hidden: false,
+    meta: {
+      title: "找回密码",
+    },
     component: () => import("@/views/login/Password.vue")
   }
 ]
@@ -25,7 +53,7 @@ const routes: Array<RouteConfig> = [
 const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
-  routes
+  routes: asyncRouterMap
 })
 
 //路由判断
--- vue-learning/vuets-app/src/views/Home.vue
+++ vue-learning/vuets-app/src/views/Home.vue
@@ -1,14 +1,23 @@
 <template>
-  <div class="home">home</div>
+  <div class="home">
+    <div class="home-body">
+      <h4 class="title">欢迎来到少林大学堂</h4>
+      <div class="des">少林学堂让你学到想学的基础知识</div>
+      <el-button @click="learn">进入学习</el-button>
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
-@Component ({
-  components: {}
+@Component({
+  components: {},
 })
-
-export default class Home extends Vue {}
+export default class Home extends Vue {
+  learn(): void {
+    window.open("https://github.com/codevvvv9/wu-FE-interview", "_blank");
+  }
+}
 </script>
 
 <style scoped></style>
