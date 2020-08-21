ef3714e5b779e2acc3ee171710f37c2a10114023
wushaolin <15265249516@163.com>
2020-08-21 23:48:27 +0800

    What:  init DataCharts.vue
    Why:
    
    *
    
    How:
    
    *

--- vue-learning/vuets-app/src/views/DataManager/DataCharts.vue
+++ vue-learning/vuets-app/src/views/DataManager/DataCharts.vue
@@ -1,15 +1,26 @@
 <template>
   <div>
-    datacharts
+    <el-tabs type="border-card" v-model="tabPosition">
+      <el-tab-pane label="折线图" name="line">
+        折线图
+      </el-tab-pane>
+      <el-tab-pane label="柱状图" name="bar">
+
+      </el-tab-pane>
+      <el-tab-pane label="饼图" name="pie">
+
+      </el-tab-pane>
+    </el-tabs>
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from "vue-property-decorator";
+import { Component, Vue, Provide } from "vue-property-decorator";
 @Component({
   components: {},
 })
 export default class DataCharts extends Vue {
+  @Provide() tabPosition: String = "line"
 }
 </script>
 
