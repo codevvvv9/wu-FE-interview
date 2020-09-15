b0e18308473a1baead54d3747f4273dc4c1dd25e
wushaolin <15265249516@163.com>
2020-09-15 23:57:22 +0800

    What: :tada: init fanyi with nodejs
    Why:
    
    *
    
    How:
    
    *

---
+++ learning-nodejs/fanyi/README.md
@@ -0,0 +1,7 @@
+## 安装相关依赖
+
+```javascript
+yarn init
+yarn add commander
+yarn add --dev @types/node
+```
---
+++ learning-nodejs/fanyi/src/cli.ts
@@ -0,0 +1,7 @@
+import * as commander from 'commander';
+
+const program = new commander.Command()
+
+program.version('0.0.1')
+
+program.parse(process.argv);
