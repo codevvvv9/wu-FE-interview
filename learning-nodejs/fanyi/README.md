cdd3ad496002b9849cf8710634719ae3a55d6f94
wushaolin <15265249516@163.com>
2020-09-16 23:26:15 +0800

    What: use baidu fanyi API and https module
    Why:
    
    *
    
    How:
    
    *

--- learning-nodejs/fanyi/README.md
+++ learning-nodejs/fanyi/README.md
@@ -5,3 +5,17 @@ yarn init
 yarn add commander
 yarn add --dev @types/node
 ```
+
+## `commander`的基本使用
+
+`commander`是`tj`大神写的命令行库，基本语法如下：
+
+```javascript
+const program = new commander.Command() //定义对象
+
+program.version('0.0.1') //定义版本号
+  .name('fy') //定义命令的默认名字
+  .usage('<english>') //定义命令的用法 尖括号是必选，[]是可选
+
+program.parse(process.argv); //解析命令行的参数
+```
--- learning-nodejs/fanyi/src/main.ts
+++ learning-nodejs/fanyi/src/main.ts
@@ -1,1 +1,29 @@
-console.log('ts');
+import * as https from 'https';
+
+export const translate = (word: string) => {
+  console.log("translate -> word", word);
+
+  const options = {
+    hostname: "www.baidu.com",
+    port: 443,
+    path: "/",
+    method: "GET",
+  };
+
+  const req = https.request(
+    options,
+    (res: any) => {
+      console.log("statusCode:", res.statusCode);
+      console.log("headers:", res.headers);
+
+      res.on("data", (d: string | Uint8Array) => {
+        process.stdout.write(d);
+      });
+    }
+  );
+
+  req.on("error", (e: any) => {
+      console.error(e);
+    });
+  req.end();
+};
