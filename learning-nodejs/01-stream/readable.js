ea5db5de81292b1dc85eaf6c70bee7fab7808699
wushaolin <15265249516@163.com>
2020-10-15 23:33:26 +0800

    What: simple Writable and Readable Stream
    Why:
    
    *
    
    How:
    
    *

---
+++ learning-nodejs/01-stream/readable.js
@@ -0,0 +1,10 @@
+const { Readable } = require('stream')
+
+const inStream = new Readable()
+
+inStream.push("ABCDEFGHIJKLM")
+inStream.push("NOPQRSTUVWXYZ")
+
+inStream.push(null)
+
+inStream.pipe(process.stdout)
