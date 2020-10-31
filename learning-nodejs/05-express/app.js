1c15636fd637f681f0b76890e3af64f728fa9708
wushaolin <15265249516@163.com>
2020-10-31 22:37:54 +0800

    What: learn express's API
    Why:
    
    *
    
    How:
    
    *

---
+++ learning-nodejs/05-express/app.js
@@ -0,0 +1,29 @@
+/**
+ * @author wushaolin
+ * @email wslsdust@163.com
+ * @create date 2020-10-31 17:53:04
+ * @modify date 2020-10-31 17:53:04
+ * @desc [学习express.xxx系列的7个API]
+ */
+
+//express.xxx系列api共有7个:1个 express() 6个方法
+const express = require('express')
+
+const app = express() //1、构建应用
+const PORT = 3000 
+
+app.use(express.json()) //2、解析发送的json文件,然后request.body会是一个object
+app.use(express.static('static')) //3、解析静态文件，默认会优先从设定的这个目录下寻找内容
+
+app.use((request, response, next) => {
+  console.log("request", request.body)
+  // request.on('data', (chunk) => {
+  //   console.log(chunk.toString());
+  // })
+  response.send('hi')
+  response.end()
+})
+
+app.listen(PORT, () => {
+  console.log(`listen ${PORT}`);
+})
