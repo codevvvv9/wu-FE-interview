ac98edcb541473bb0926c40c4da87e48c6fd8478
wushaolin <15265249516@163.com>
2020-11-02 23:26:44 +0800

    What: learn express's app.set
    Why:
    
    *
    
    How:
    
    *

--- learning-nodejs/05-express/app.js
+++ learning-nodejs/05-express/app.js
@@ -12,9 +12,16 @@ const express = require('express')
 const app = express() //1、构建应用
 const PORT = 3000 
 
-app.set('case sensitive routing', true) //必须设置在最开始，是保证项目的路由是否强制大小写，默认为false，大小写一样
+app.set('case sensitive routing', true) //1、必须设置在最开始，是保证项目的路由是否强制大小写，默认为false，大小写一样
+app.set("views", "views")  //2、应用使用的视图在哪个目录
+app.set("view engine", "ejs") //3、模板引擎使用哪一个
 app.use(express.json()) //2、解析发送的json文件,然后request.body会是一个object
 app.use(express.static('static')) //3、解析静态文件，默认会优先从设定的这个目录下寻找内容
+app.use(express.urlencoded()) //
+
+app.get('/test', (req, res, next) => {
+  res.render('demo', {pageTitle: "EJS测试"}) //render的第一个参数就是views里面第一个文件的名字
+})
 
 app.get('/style.css', (req, res, next) => {
   res.send('style.css')
