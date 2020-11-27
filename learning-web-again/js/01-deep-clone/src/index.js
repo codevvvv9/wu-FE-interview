23262c28488a8bb8017611200b24767a9b75d12e
wushaolin <15265249516@163.com>
2020-11-27 23:30:04 +0800

    What: 使用ES6的class语法重写深克隆
    Why:
    
    *
    
    How: 写一个DeepClone类，cache就可以作为私有属性了
    
    *

--- learning-web-again/js/01-deep-clone/src/index.js
+++ learning-web-again/js/01-deep-clone/src/index.js
@@ -1,44 +1,46 @@
-let cache = [];
-function deepClone(source) {
-  if (source instanceof Object) {
-    let cachedSource = findCache(source);
-    if (cachedSource) {
-      return cachedSource;
-    } else {
-      let newSource;
-      if (source instanceof Array) {
-        newSource = [];
-      } else if (source instanceof Function) {
-        newSource = function () {
-          return source.apply(this, arguments);
-        };
-      } else if(source instanceof RegExp) {
-        newSource = new RegExp(source.source, source.flags)
-      } else if (source instanceof Date) {
-        newSource = new Date(source)
-      }else {
-        newSource = {};
-      }
-      cache.push([source, newSource]);
-      for (const key in source) {
-        if (source.hasOwnProperty(key)) {
-          const element = source[key];
-          newSource[key] = deepClone(element);
-        }
-      }
-      return newSource;
-    }
+class DeepClone {
+  constructor() {
+    this.cache = [];
   }
-  return source;
-
-  function findCache(source) {
-    for (let i = 0; i < cache.length; i++) {
-      if (cache[i][0] === source) {
-        return cache[i][1];
+  deepClone(source) {
+    if (source instanceof Object) {
+      let cachedSource = this.findCache(source);
+      if (cachedSource) {
+        return cachedSource;
+      } else {
+        let newSource;
+        if (source instanceof Array) {
+          newSource = [];
+        } else if (source instanceof Function) {
+          newSource = function () {
+            return source.apply(this, arguments);
+          };
+        } else if (source instanceof RegExp) {
+          newSource = new RegExp(source.source, source.flags);
+        } else if (source instanceof Date) {
+          newSource = new Date(source);
+        } else {
+          newSource = {};
+        }
+        this.cache.push([source, newSource]);
+        for (const key in source) {
+          if (source.hasOwnProperty(key)) {
+            const element = source[key];
+            newSource[key] = this.deepClone(element);
+          }
+        }
+        return newSource;
+      }
+    }
+    return source;
+  }
+  findCache(source) {
+    for (let i = 0; i < this.cache.length; i++) {
+      if (this.cache[i][0] === source) {
+        return this.cache[i][1];
       }
     }
     return undefined;
   }
 }
-
-module.exports = deepClone;
+module.exports = DeepClone;
--- learning-web-again/js/01-deep-clone/test/index.spec.js
+++ learning-web-again/js/01-deep-clone/test/index.spec.js
@@ -2,47 +2,47 @@ const { assert } = require("chai");
 const chai = require("chai");
 const sinon = require("sinon");
 const sinonChai = require("sinon-chai");
-const deepClone = require("../src/index");
+const DeepClone = require("../src/index");
 chai.use(sinonChai);
 
-describe("deepClone", () => {
+describe("DeepClone", () => {
   it("deepClone是一个函数", () => {
-    // assert(typeof deepClone === "function");
-    assert.isFunction(deepClone)
+    // assert(typeof DeepClone === "function");
+    assert.isFunction(DeepClone)
   });
   it("deepClone可以克隆6种原始数据类型", () => {
     //1.Number
     const num1 = 123;
-    const num2 = deepClone(num1);
+    const num2 = new DeepClone().deepClone(num1);
     assert(num1 === num2);
     //2.String
     const str = 123;
-    const str1 = deepClone(str);
+    const str1 = new DeepClone().deepClone(str);
     assert(str === str1);
     //3.Boolean
     const bool1 = true;
-    const bool2 = deepClone(bool1);
+    const bool2 = new DeepClone().deepClone(bool1);
     assert(bool1 === bool2);
     const bool3 = false;
-    const bool4 = deepClone(bool3);
+    const bool4 = new DeepClone().deepClone(bool3);
     assert(bool3 === bool4);
     //4. Undefined
     const undefine1 = undefined;
-    const undefine2 = deepClone(undefine1);
+    const undefine2 = new DeepClone().deepClone(undefine1);
     assert(undefine1 === undefine2);
     //5. Null
     const null1 = null;
-    const null2 = deepClone(null1);
+    const null2 = new DeepClone().deepClone(null1);
     assert(null1 === null2);
     //6. Symbol
     const symbol1 = Symbol();
-    const symbol2 = deepClone(symbol1);
+    const symbol2 = new DeepClone().deepClone(symbol1);
     assert(symbol1 === symbol2);
   });
 
   it("deepClone能克隆普通对象", () => {
     const obj = { name: "wushao", child: { name: "little" } };
-    const obj1 = deepClone(obj);
+    const obj1 = new DeepClone().deepClone(obj);
     assert(obj !== obj1);
     assert(obj.name === obj1.name);
     assert(obj.child !== obj1.child);
@@ -54,7 +54,7 @@ describe("deepClone", () => {
       [11, 22, 33],
       [111, 222, 333],
     ];
-    const arr1 = deepClone(arr);
+    const arr1 = new DeepClone().deepClone(arr);
     assert(arr !== arr1);
     assert(arr[0] !== arr1[0]);
     assert(arr[1] !== arr1[1]);
@@ -67,7 +67,7 @@ describe("deepClone", () => {
       return x + y;
     };
     fun1.xxx = { yyy: { zzz: 1 } };
-    const fun2 = deepClone(fun1);
+    const fun2 = new DeepClone().deepClone(fun1);
 
     assert(fun1 !== fun2);
     assert(fun1.xxx !== fun2.xxx);
@@ -79,7 +79,7 @@ describe("deepClone", () => {
   it("deepClone能克隆环", () => {
     const obj = {name: 'wushao'}
     obj.self = obj
-    const obj1 = deepClone(obj)
+    const obj1 = new DeepClone().deepClone(obj)
     assert(obj !== obj1)
     assert(obj.name === obj1.name)
     assert(obj.self !== obj1.self)
@@ -90,7 +90,7 @@ describe("deepClone", () => {
     //等价于
     const regExp = new RegExp("hi\\d+", "gi")
     regExp.xxx = {yyy: {zzz: 1}}
-    const regExp1 = deepClone(regExp)
+    const regExp1 = new DeepClone().deepClone(regExp)
     assert(regExp !== regExp1)
     assert(regExp.xxx !== regExp1.xxx)
     assert(regExp.xxx.yyy !== regExp1.xxx.yyy)
@@ -102,7 +102,7 @@ describe("deepClone", () => {
   it("能够克隆Date", () => {
     const date = new Date()
     date.xxx = {yyy: {zzz: 1}}
-    const date1 = deepClone(date)
+    const date1 = new DeepClone().deepClone(date)
     assert(date !== date1)
     assert(date.xxx !== date1.xxx)
     assert(date.xxx.yyy !== date1.xxx.yyy)
@@ -112,7 +112,7 @@ describe("deepClone", () => {
   it("能够跳过原型属性不克隆", () => {
     const obj1 = Object.create({name: "a"})
     obj1.xxx = {yyy: {zzz: 1}}
-    const obj2 = deepClone(obj1)
+    const obj2 = new DeepClone().deepClone(obj1)
     assert(obj1 !== obj2)
     assert.isTrue("name" in obj1)
     assert.isFalse("name" in obj2)
