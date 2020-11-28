893ec442990a9a22cdff534724959e0e0b69bac6
wushaolin <15265249516@163.com>
2020-11-28 21:39:58 +0800

    What: webpack多文件写法
    Why:
    
    *
    
    How:
    
    *

--- learning-webpack/my-webpack/src/index.js
+++ learning-webpack/my-webpack/src/index.js
@@ -1,3 +1,4 @@
 import { helloWorld } from "./helloworld.js";
 
 document.write(helloWorld())
+console.log(`${process.env.NODE_ENV}`);
--- learning-webpack/my-webpack/webpack.config.js
+++ learning-webpack/my-webpack/webpack.config.js
@@ -1,12 +1,31 @@
+/**
+ * @author wushaolin
+ * @email wslsdust@163.com
+ * @create date 2020-11-28 19:22:54
+ * @modify date 2020-11-28 19:22:54
+ * @desc [学习webpack的5个核心概念：entry、output、loader、plugin、mode]
+ */
+
 "use strict";
 
 const path = require("path");
 
 module.exports = {
-  entry: "./src/index.js",
-  output: {
-    path: path.join(__dirname, "dist"),
-    filename: "bundle.js",
+  //1、单入口文件写法如下：
+  // entry: "./src/index.js",
+  // output: {
+  //   path: path.join(__dirname, "dist"),
+  //   filename: "bundle.js",
+  // },
+  //2、多入口文件写法如下：entry 使用对象的写法，output只能使用占位符[name]
+  entry: {
+    'index': './src/index.js',
+    'search': './src/search.js'
   },
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: '[name].js'
+  },
+  //3、mode有三个值：production(默认值)、development、none，开启之后会启动相应的默认配置函数
   mode: "production"
 };
