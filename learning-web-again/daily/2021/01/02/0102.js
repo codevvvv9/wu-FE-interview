4382667f48da5ee61e792290e195aa26c42a71ee
wushaolin <15265249516@163.com>
2021-01-02 22:59:57 +0800

    What: MutationObserver API 入门
    Why:
    
    *
    
    How:
    
    *

---
+++ learning-web-again/daily/2021/01/02/0102.js
@@ -0,0 +1,16 @@
+//MutationObserver是监视DOM变化的异步API
+let mutationObserver = new MutationObserver(function (mutations, observer) {
+  mutations.forEach(mutation => {
+    console.log('mutation is', mutation);
+  })
+})
+
+//实例方法1 observe()
+//接受的第一个参数所要观察的DOM节点
+let article = document.querySelector('article')
+//接受的第二个参数 一个配置对象，指定要观察的特定运动
+let options = {
+  'childList': true,
+  'attributes': true,
+}
+mutationObserver.observe(article, options)
